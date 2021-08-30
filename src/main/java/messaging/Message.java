f31b4d083c063ced86c4c49e879a1be4dce2e822
Caleb <ccarlson355@gmail.com>
2021-08-30 09:48:43 -0600

    DF-5
    * Add typeFromInteger() function and corresponding unit tests

--- src/main/java/messaging/Message.java
+++ src/main/java/messaging/Message.java
@@ -2,9 +2,34 @@ package messaging;
 
 public class Message {
 
-    public enum Type {
+    public enum MessageType {
         HEARTBEAT_MINOR, HEARTBEAT_MAJOR
     }
 
+    public final MessageType type; // once set, this shouldn't change
 
+    public Message() {
+        this(MessageType.HEARTBEAT_MINOR); // default to type 0
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    /**
+     * Converts an integer to a MessageType enum
+     * @param type integer type
+     * @return MessageType enum
+     */
+    public static MessageType typeFromInteger(int type) {
+        switch (type) {
+            case 0: return MessageType.HEARTBEAT_MINOR;
+            case 1: return MessageType.HEARTBEAT_MAJOR;
+            default: return null;
+        }
+    }
 }
--- src/test/java/messaging/MessageTest.java
+++ src/test/java/messaging/MessageTest.java
@@ -1,5 +1,45 @@
 package messaging;
 
+import org.junit.jupiter.api.Test;
+import messaging.Message.MessageType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
 public class MessageTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        Message message = new Message();
+        MessageType expected = MessageType.HEARTBEAT_MINOR;
+        MessageType actual = message.getType();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testArgsConstructor() {
+        Message message = new Message(MessageType.HEARTBEAT_MAJOR);
+        MessageType expected = MessageType.HEARTBEAT_MAJOR;
+        MessageType actual = message.getType();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTypeFromIntegerValid() {
+        int[] types = new int[]{0, 1};
+        MessageType[] expecteds = new MessageType[]{MessageType.HEARTBEAT_MINOR, MessageType.HEARTBEAT_MAJOR};
+        for (int i = 0; i < types.length; ++i) {
+            MessageType expected = expecteds[i];
+            MessageType actual = Message.typeFromInteger(types[i]);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void testTypeFromIntegerInvalid() {
+        assertNull(Message.typeFromInteger(-1));
+        assertNull(Message.typeFromInteger(MessageType.values().length));
+    }
 
 }
