6b56d9c310b96441efdfd1587e6b5372f1683137
Caleb Carlson <ccarlson355@gmail.com>
2021-09-01 22:07:02 -0600

    DF-16
    * Add Constants for storage abbreviations
    * Implement FileLoader for retrieving chunk-sized byte arrays
    * Unit test FileLoader

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -7,7 +7,15 @@ package util;
  */
 public class Constants {
 
+    // Human-readable storage constants
+    public static final short BYTE = 1;
+    public static final short KiB = 1024 * BYTE, KB = 1000 * BYTE;
+    public static final int   MiB = 1024 * KiB, MB = 1000 * KB;
+    public static final long  GiB = 1024 * MiB, GB = 1000 * MB;
+    public static final long  TiB = 1024 * GiB, TB = 1000 * GB;
+
+    // Service-specific constants
     public static final int CONTROLLER_PORT = 9001;
     public static final int CHUNK_SERVER_PORT = 9000;
-
+    public static final int CHUNK_SIZE = 64 * KB;
 }
