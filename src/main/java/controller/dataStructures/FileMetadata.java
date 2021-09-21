29adab86d51991a051b917400aca14ce83178fb9
Matthew Ernst <matthew.f.ernst@gmail.com>
2021-09-20 23:33:17 -0600

    resturcture and testing

--- src/main/java/controller/dataStructures/FileMetadata.java
+++ src/main/java/controller/dataStructures/FileMetadata.java
@@ -4,5 +4,29 @@ import java.util.Vector;
 
 public class FileMetadata {
     public String absolutePath;
-    public Vector<Vector<ChunkServerMetadata>> chunksServers; // vector is synchronized
+    public Vector<Vector<ChunkServerMetadata>> chunksServers;
+
+    public FileMetadata(String absolutePath) {
+        this.absolutePath = absolutePath;
+        this.chunksServers = new Vector<>();
+    }
+
+    public FileMetadata(String absolutePath, Vector<Vector<ChunkServerMetadata>> chunksServers) {
+        this.absolutePath = absolutePath;
+        this.chunksServers = chunksServers;
+    }
+
+    public boolean equals(Object other) {
+        if (other == null)
+            return false;
+        if (other == this)
+            return true;
+        if (other instanceof String)
+            return ((String) other).equals(this.absolutePath);
+        if (!(other instanceof FileMetadata))
+            return false;
+        FileMetadata otherFileMetadata = (FileMetadata) other;
+        return this.absolutePath.equals(otherFileMetadata.absolutePath);
+    }
+
 }
